0e42ea028049cd4bd4ab7f841382584f0394b3ad
IgrD08 <dementevsuper2008@gmail.com>
2026-09-18 00:10:44 +0300

    first

---
+++ oneginSorting.cpp
@@ -0,0 +1,94 @@
+#include <stdio.h>
+#include <assert.h>
+#include <string.h>
+#include <stdint.h>
+#include <ctype.h>
+#include <stdlib.h>
+#include "stringFunctions.cpp"
+#include "sorting.cpp"
+
+const int MAX_STR = 10000;
+
+int readFromFile(char **text, char **index, const char *fileName);
+void writeToFile(char **index, int numberOfReadLines, FILE *filePointerLines);
+
+int main()
+{
+    char *text[MAX_STR] = {};
+    char *index[MAX_STR] = {};
+
+    int nLines = readFromFile(text, index, "onegin.txt");
+
+    FILE *filePointerWrite = fopen("reonegin.txt", "w");
+
+    if (filePointerWrite == NULL)
+    {
+        fprintf(stderr, "Unable to open %s\n", "reonegin.txt");
+        return 0;
+    }
+
+    qSort(index, nLines, strComparatorDown, sizeof(char*));
+
+    writeToFile(index, nLines, filePointerWrite);
+
+    qSort(index, nLines, strComparatorFromEnd, sizeof(char*));
+
+    writeToFile(index, nLines, filePointerWrite);
+
+    fclose(filePointerWrite);
+
+    return 0;
+}
+
+int readFromFile(char **text, char **index, const char *fileName)
+{
+    assert(fileName);
+    assert(index);
+    assert(text);
+
+    FILE *filePointerRead = fopen(fileName, "r");
+    int numberOfReadLines = 0;
+
+    if (filePointerRead == NULL)
+    {
+        fprintf(stderr, "Unable to open %s\n", fileName);
+        return -2;
+    }
+
+    char buffer[MAX_STR] = "";
+
+    while(numberOfReadLines < MAX_STR && fgets(buffer, sizeof(buffer), filePointerRead) != NULL)
+    {
+        size_t lenBuffer = strlen(buffer);
+
+        if (lenBuffer > 0 && buffer[lenBuffer - 1] == '\n')
+        {
+            buffer[lenBuffer - 1] = '\0';
+        }
+
+        text[numberOfReadLines] = strdup(buffer);
+
+        index[numberOfReadLines] = text[numberOfReadLines];
+
+        numberOfReadLines++;
+    }
+
+    fclose(filePointerRead);
+
+    return numberOfReadLines;
+}
+
+void writeToFile(char **index, int numberOfReadLines, FILE *filePointerWrite)
+{
+    assert(index);
+    assert(filePointerWrite);
+
+    for (int i = 0; i < numberOfReadLines; i++)
+    {
+        fprintf(filePointerWrite, "<<%s>>\n", index[i]);
+    }
+
+    fprintf(filePointerWrite, "*****************************************\n");
+
+    return;
+}
---
+++ sorting.cpp
@@ -0,0 +1,110 @@
+void swap(void *firstValue, void *secondValue, size_t sizeType);
+void qSort(void *array, size_t arrayLen,
+           int (*comparator)(const void* first, const void* second), size_t sizeType);
+int strComparatorDown(const void *first, const void *second);
+int strComparatorFromEnd(const void *first, const void *second);
+
+void qSort(void *array, size_t arrayLen,
+           int (*comparator)(const void* first, const void* second), size_t sizeType)
+{
+    assert(array);
+
+    if (arrayLen <= 1) return;
+
+    uint8_t *last = (uint8_t*)array + (arrayLen - 1) * sizeType;
+    size_t quantityLessLast = 0;
+
+    for (size_t i = 0; i < arrayLen - 1; i++)
+    {
+        if (comparator((uint8_t*)array + i * sizeType, last) > 0)
+        {
+            swap((uint8_t*)array + quantityLessLast * sizeType, (uint8_t*)array + i * sizeType, sizeType);
+            quantityLessLast++;
+        }
+    }
+    swap((uint8_t*)array + quantityLessLast * sizeType, (uint8_t*)array + (arrayLen - 1) * sizeType, sizeType);
+
+    qSort((uint8_t*)array, quantityLessLast, comparator, sizeType);
+
+    qSort((uint8_t*)array + (quantityLessLast + 1) * sizeType, arrayLen - quantityLessLast - 1, comparator, sizeType);
+
+    return;
+}
+
+void swap(void *firstValue, void *secondValue, size_t sizeType)
+{
+    assert(firstValue);
+    assert(secondValue);
+
+    char temp = 0;
+
+    for (size_t i = 0; i < sizeType; i++)
+    {
+        memcpy(&temp, ((char*)firstValue) + i, 1);
+        memcpy(((char*)firstValue) + i, ((char*)secondValue) + i, 1);
+        memcpy(((char*)secondValue + i), &temp, 1);
+    }
+
+    return;
+}
+
+int strComparatorDown(const void *first, const void *second)
+{
+    assert(first);
+    assert(second);
+
+    const char *firstString = *((const char**)first);
+    const char *secondString = *((const char**)second);
+    int firstCounter = 0, secondCounter = 0;
+
+    while ((firstString[firstCounter]) != '\0' &&
+           (secondString[secondCounter]) != '\0')
+    {
+        while (!isalpha(firstString[firstCounter]) && firstString[firstCounter] != '\0') firstCounter++;
+        while (!isalpha(secondString[secondCounter]) && secondString[secondCounter] != '\0') secondCounter++;
+
+        if (firstString[firstCounter] == '\0' || secondString[secondCounter] == '\0')
+        {
+            break;
+        }
+
+        if (tolower(firstString[firstCounter]) != tolower(secondString[secondCounter]))
+        {
+            return secondString[secondCounter] - firstString[firstCounter];
+        }
+
+        firstCounter++;
+        secondCounter++;
+    }
+
+    return secondString[secondCounter] - firstString[firstCounter];
+}
+
+int strComparatorFromEnd(const void *first, const void *second)
+{
+    assert(first);
+    assert(second);
+
+    const char *firstString = *((const char**)first);
+    const char *secondString = *((const char**)second);
+    int firstCounter = 0, secondCounter = 0;
+
+    while ((firstString[firstCounter]) != '\0') firstCounter++;
+
+    while ((secondString[secondCounter]) != '\0') secondCounter++;
+
+    int i = firstCounter - 1;
+    int j = secondCounter - 1;
+    for (; i >= 0, j >= 0; i--, j--)
+    {
+        while (!isalpha(firstString[i]) && i >= 1) i--;
+        while (!isalpha(secondString[j]) && j >= 1) j--;
+
+        if (tolower(firstString[i]) != tolower(secondString[j]))
+        {
+            return secondString[i] - firstString[j];
+        }
+    }
+
+    return secondString[i] - firstString[j];
+}
