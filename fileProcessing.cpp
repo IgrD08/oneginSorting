39f5db6b25e8ad7415dc62a710fed4cf877b7f42
IgrD08 <dementevsuper2008@gmail.com>
2026-09-21 20:15:06 +0300

    улучшено разбиение на файлы

---
+++ fileProcessing.cpp
@@ -0,0 +1,100 @@
+const int MAX_STR = 10000;
+
+int readFromFile(char **index, const char *fileName, int flag, char **bufferPtr);
+void writeToFile(char **index, int numberOfReadLines, int fileDescriptor);
+int fileOpening(const char *fileName, int flag);
+char* strchrMy(char *str, int ch);
+
+int readFromFile(char **index, const char *fileName, int flag, char **bufferPtr)
+{
+    assert(fileName);
+    assert(index);
+    assert(bufferPtr);
+
+    int fileDescriptorRead = fileOpening(fileName, flag);//TODO можно ли возвращать код ошибки
+    if (fileDescriptorRead < 0) return -1;
+
+    struct stat fileInfo;
+    if (stat(fileName, &fileInfo) == -1) return -1;
+
+    *bufferPtr = (char*) (calloc(fileInfo.st_size + 1, sizeof(char)));
+
+    ssize_t a = read(fileDescriptorRead, *bufferPtr, fileInfo.st_size + 1);
+    if (a == -1) return -1;
+
+    (*bufferPtr)[fileInfo.st_size] = '\0';
+
+    if (close(fileDescriptorRead) == -1) return -1;
+
+    int numberOfReadLines = 0;
+
+    index[numberOfReadLines] = *bufferPtr;
+    numberOfReadLines++;
+
+    char *element = *bufferPtr;
+
+    while ((element = strchrMy(element, '\n')) != NULL)//TODO - проход по строке
+    {
+        *element = '\0';
+
+        char *nextLine = element + 1;
+
+        if (*nextLine != '\0' && numberOfReadLines < MAX_STR)
+        {
+            index[numberOfReadLines] = nextLine;
+            numberOfReadLines++;
+        }
+
+        element = nextLine;
+    }
+
+    return numberOfReadLines;
+}
+
+void writeToFile(char **index, int numberOfReadLines, int fileDescriptor)
+{
+    assert(index);
+
+    for (int i = 0; i < numberOfReadLines; i++)
+    {
+        if (index[i] != NULL)
+        {
+            write(fileDescriptor, index[i], strlen(index[i]));
+            write(fileDescriptor, "\n", 1);
+        }
+    }
+
+    const char *text = "*****************************************\n";
+
+    write(fileDescriptor, text, 42);
+
+    return;
+}
+
+int fileOpening(const char *fileName, int flag)
+{
+    int fileDescriptor = open(fileName, flag);
+
+    if (fileDescriptor == -1)
+    {
+        fprintf(stderr, "Unable to open %s\n", fileName);
+
+        return fileDescriptor;
+    }
+
+    return fileDescriptor;
+}
+
+char* strchrMy(char *str, int ch)
+{
+
+    while (*str != '\0')
+    {
+
+        if (*str == ch)
+            return str;
+
+        str++;
+    }
+    return NULL;
+}
--- oneginSorting.cpp
+++ oneginSorting.cpp
@@ -8,16 +8,16 @@
 #include <stdlib.h>
 #include <sys/stat.h>
 #include "sorting.cpp"
-
-const int MAX_STR = 10000;
-
-int readFromFile(char **index, const char *fileName, int flag, char **bufferPtr);
-void writeToFile(char **index, int numberOfReadLines, int fileDescriptor);
-int fileOpening(const char *fileName, int flag);
-char* strchrMy(char *str, int ch);
+#include "fileProcessing.cpp"
 
 int main()
 {
+    struct arrayParameter
+    {
+        char *array;
+        size_t arrayLen;
+    };
+
     char *index[MAX_STR] = {NULL};
     char *buffer = NULL;
 
@@ -41,97 +41,5 @@ int main()
 
     free(buffer); //TODO - прокачать free
 
-
     return 0;
-}
-
-int readFromFile(char **index, const char *fileName, int flag, char **bufferPtr)
-{
-    assert(fileName);
-    assert(index);
-    assert(bufferPtr);
-
-    int fileDescriptorRead = fileOpening(fileName, flag);//TODO можно ли возвращать код ошибки
-    if (fileDescriptorRead < 0) return -1;
-
-    struct stat fileInfo;
-    stat(fileName, &fileInfo);//TODO что возвращает stat
-
-    *bufferPtr = (char*) (calloc(fileInfo.st_size + 1, sizeof(char)));
-
-    read(fileDescriptorRead, *bufferPtr, fileInfo.st_size + 1);
-    (*bufferPtr)[fileInfo.st_size] = '\0';
-    close(fileDescriptorRead);
-
-    int numberOfReadLines = 0;
-
-    index[numberOfReadLines] = *bufferPtr;
-    numberOfReadLines++;
-
-    char *element = *bufferPtr;
-
-    while ((element = strchr(element, '\n')) != NULL)//TODO - проход по строке
-    {
-        *element = '\0';
-
-        char *nextLine = element + 1;
-
-        if (*nextLine != '\0' && numberOfReadLines < MAX_STR)
-        {
-            index[numberOfReadLines] = nextLine;
-            numberOfReadLines++;
-        }
-
-        element = nextLine;
-    }
-
-    return numberOfReadLines;
-}
-
-void writeToFile(char **index, int numberOfReadLines, int fileDescriptor)
-{
-    assert(index);
-
-    for (int i = 0; i < numberOfReadLines; i++)
-    {
-        if (index[i] != NULL)
-        {
-            write(fileDescriptor, index[i], strlen(index[i]));
-            write(fileDescriptor, "\n", 1);
-        }
-    }
-
-    const char *text = "*****************************************\n";
-
-    write(fileDescriptor, text, 42);
-
-    return;
-}
-
-int fileOpening(const char *fileName, int flag)
-{
-    int fileDescriptor = open(fileName, flag);
-
-    if (fileDescriptor == -1)
-    {
-        fprintf(stderr, "Unable to open %s\n", fileName);
-
-        return fileDescriptor;
-    }
-
-    return fileDescriptor;
-}
-
-char* strchrMy(char *str, int ch)
-{
-
-    while (*str != '\0')
-    {
-
-        if (*str == ch)
-            return str;
-
-        str++;
-    }
-    return NULL;
 }
